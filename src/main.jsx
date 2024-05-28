7667f17c09aeb1107508826045e7c56b745ae337
Sandra Llanos <sandra.llanos.alvarez@gmail.com>
2024-05-27 22:10:39 -0500

    useState

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { HooksApp } from './HooksApp'
+import { CounterApp } from './01-useState/CounterApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <HooksApp /> */}
+    <CounterApp />
+  </React.StrictMode>,
+)
