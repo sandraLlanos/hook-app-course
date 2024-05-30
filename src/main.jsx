56fdfca6122d2755866fb0eff9252f37f9665df1
Sandra Llanos <sandra.llanos.alvarez@gmail.com>
2024-05-29 23:20:20 -0500

    Form with custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
     {/* <CounterWithCustomHook /> */}
-    <SimpleForm />
+    {/* <SimpleForm /> */}
+    <FormWithCustomHook />
   </React.StrictMode>,
 )
